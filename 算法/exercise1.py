efa2159fecbf0512c7994e9a3a5fac961c24e365
lizhonghuan <2275572017@qq.com>
2017-09-26 10:00:13 +0800

    完成

--- 算法/exercise1.py
+++ 算法/exercise1.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 #1.
 #k为移动位数，n为数组长度，S为数组
 def right_move01(k, n, S):
@@ -18,3 +20,35 @@ def right_move02(k, n, S):
 	p = 0 #附加的空间
 	if k > n:
 		k = k % n;
+	while k > 0:
+		p = S[n-1]
+		for i in range(n,-1,-1):
+			S[i] = S[i-1]
+		S[0] = p
+		k -= 1
+
+
+#3.
+#逆序数组
+def reverse(S, l, r):
+	p = 0 #附加的空间	
+	while l == r:
+		p = S[l]
+		S[r] = S[l]
+		S[l] = p
+		l += 1
+		r -= 1
+
+
+def right_move03(k, n, S):
+	if k > n:
+		k = k % n;
+	reverse(S, 0, n-k-1)
+	reverse(S, n-k, n-1)
+	reverse(S, 0, n-1)
+
+
+
+
+
+
--- 算法/exercise2.py
+++ 算法/exercise2.py
@@ -14,23 +14,28 @@ def despath_name(pathname):
 
 #解压缩tar
 def unpack_path_file(pathname):
+	if not pathname.endswith("tar.gz"):
+		return;
 	archive = tarfile.open(pathname,'r:gz')
 	filenames = archive.getnames()
 	print("解压缩后的文件：" )
 	print(filenames)
 	despath = despath_name(pathname)
 	archive.extractall(despath)
+	archive.close()
 	# for filename in filenames:
 	# 	filepath = os.getcwd() + "/" + filename
 	# 	print(filepath)
 	# 	archive.extract(filename,pathname[:-7])
-	archive.close()
+	
 
 #读文件数字
 def read_file_number(pathname):
+	print("读到的文件路径：" + pathname)
 	number = 0
 	finalfile = open(pathname, 'r')
 	line = finalfile.readline()
+	print("文件内容是：" + line)
 	number = (string.atoi(line) >= 0) and string.atoi(line) or 0		
 	finalfile.close()
 	return number
@@ -38,33 +43,37 @@ def read_file_number(pathname):
 #文件操作
 def file_action(pathname):
 	global file_num
-	file_num += 1
 	global result
 	result += read_file_number(pathname)
+	if not os.path.isdir(pathname):
+		file_num += 1
 
 #递归求解
+static_num = 0
 def recurs_file(pathname):
 	unpack_path_file(pathname)
-	pathname = pathname[:-7]
+	newpath = pathname[:-7]
 
-	if os.path.isdir(pathname):
-		filelist = os.listdir(pathname)
+	if os.path.isdir(newpath):
+		filelist = os.listdir(newpath)
+		print("子文件：")
+		print(filelist)
 		for file in filelist:
-			current_path = pathname + "/" + file
+			global static_num
+			static_num += 1
+			current_path = newpath + "/" + file
 			print("当前文件：" + current_path)
 			recurs_file(current_path)
 	else:
-		file_action()
-	# if pathname.endswith("tar.gz"):
-		
-	# else:
-	# 	recurs_file(pathname)
+		file_action(newpath)
 
 
 recurs_file(sys.argv[1])
+print(static_num)
 # unpack_path_file(sys.argv[1])
 
-print("文件数是：" + '%d' %file_num)
-print("非负数的和是：" + '%d' %result)
+print("最终结果是：")
+print("文件数：" + '%d' %file_num)
+print("非负数的和：" + '%d' %result)
 
 
